108badf12c3d149138c3556278a140da71848836
Chowe <chowe0988@gmail.com>
2014-07-30 13:19:03 +0800

    guku
    tnt.html mail classs出錯 以解決

--- assets/js/second.js
+++ assets/js/second.js
@@ -1,8 +1,5 @@
 jQuery(document).ready(function($){
 	//open-close submenu on mobile
-
-	$('.cd-main-nav').on('click', function(event){
-		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	$('.tnt-nav').on('click', function(event){
 		if($(event.target).is('.tnt-nav')) $(this).children('ul').toggleClass('is-visible');
 
