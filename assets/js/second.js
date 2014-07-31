844de88fd9fb09a5bb460199bac7fdc4046ff156
chowe0988 <chowe0988@gmail.com>
2014-08-01 02:13:23 +0800

    guku
    img照片版面縮放

--- assets/js/second.js
+++ assets/js/second.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
 	//open-close submenu on mobile
-	$('.tnt-nav').on('click', function(event){
-		if($(event.target).is('.tnt-nav')) $(this).children('ul').toggleClass('is-visible');
+	$('.guku-nav').on('click', function(event){
+		if($(event.target).is('.guku-nav')) $(this).children('ul').toggleClass('is-visible');
 
 	});
 });
